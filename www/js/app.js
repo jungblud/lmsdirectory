58c1ce62f0fbe33b5a9fd20a48ac024952ba22a4
Tim Youngblood <tim@codescience.com>
2014-09-19 06:08:47 -0400

    Working on performance.

--- www/js/app.js
+++ www/js/app.js
@@ -3,6 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+angular.module('app', ['pasvaz.bindonce']);
 
 var lmsApp = angular.module('lmsApp', ['ionic'])
 
@@ -26,5 +27,4 @@ lmsApp.controller('lmsAppCtrl', ['$scope', '$http', function ($scope, $http){
   });
 
 }]);
-
 
